3651189d4fb058cd18016599c59d6eddb51a1b5f
fuyanpeng <fuyanpeng@vipkid.com.cn>
2021-07-27 16:50:56 +0800

    climb staris

---
+++ src/_0070climbstairs/ClimbStairs.java
@@ -0,0 +1,16 @@
+package _0070climbstairs;
+
+public class ClimbStairs {
+    public int clibstairs(int n){
+        int[] dp = new int[n+1];
+        if (n == 1){
+            return 1;
+        }
+        dp[1]=1;
+        dp[2]=2;
+        for (int i=3;i<n+1;i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
+}
